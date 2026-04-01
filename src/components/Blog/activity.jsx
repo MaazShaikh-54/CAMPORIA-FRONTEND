b1b5fa2bfe44ceb34dedea21acdaadf8fe27bbad
arfanrabs <arfan.rabs@gmail.com>
2026-04-01 16:36:27 +0530

    Optimize image loading

--- src/components/Blog/activity.jsx
+++ src/components/Blog/activity.jsx
@@ -5,41 +5,41 @@ export default function Activity() {
         <h1>Activities</h1>
         <section className="Campfire activity-section">
           <h2>Camp fire</h2><p><i> - Gather around the warm glow of a crackling fire, sharing stories under the night sky.</i></p>
-            <img src="/assets/activities/campfire1.jpg" alt="Camp Fire - activity" loading="lazy" />
-            <img src="/assets/activities/campfire2.jpg" alt="Camp Fire - activity" loading="lazy" />
-            <img src="/assets/activities/campfire3.jpg" alt="Camp Fire - activity" loading="lazy" />
-            <img src="/assets/activities/campfire4.jpg" alt="Camp Fire - activity" loading="lazy" />
-            <img src="/assets/activities/campfire5.jpg" alt="Camp Fire - activity" loading="lazy" />
+            <img src="/assets/activities/campfire1.jpg" alt="Camp Fire - activity" loading="lazy" decoding="async"  />
+            <img src="/assets/activities/campfire2.jpg" alt="Camp Fire - activity" loading="lazy" decoding="async"  />
+            <img src="/assets/activities/campfire3.jpg" alt="Camp Fire - activity" loading="lazy" decoding="async"  />
+            <img src="/assets/activities/campfire4.jpg" alt="Camp Fire - activity" loading="lazy" decoding="async"  />
+            <img src="/assets/activities/campfire5.jpg" alt="Camp Fire - activity" loading="lazy" decoding="async"  />
         </section>
         <section className="Cycling activity-section">
           <h2>Cycling</h2><p><i> - Ride through scenic trails, enjoying the fresh air and breathtaking landscapes.</i></p>
-            <img src="/assets/activities/cycling1.jpg" alt="Cycling - activity" loading="lazy" />
-            <img src="/assets/activities/cycling2.jpg" alt="Cycling - activity" loading="lazy" />
+            <img src="/assets/activities/cycling1.jpg" alt="Cycling - activity" loading="lazy" decoding="async"  />
+            <img src="/assets/activities/cycling2.jpg" alt="Cycling - activity" loading="lazy" decoding="async"  />
         </section>
         <section className="Hiking activity-section">
           <h2>Hiking</h2><p><i> - Trek across rugged terrains, discovering hidden waterfalls and stunning viewpoints.</i></p>
-            <img src="/assets/activities/hiking1.jpg" alt="Hiking - activity" loading="lazy" />
-            <img src="/assets/activities/hiking2.jpg" alt="Hiking - activity" loading="lazy" />
-            <img src="/assets/activities/hiking3.jpg" alt="Hiking - activity" loading="lazy" />
+            <img src="/assets/activities/hiking1.jpg" alt="Hiking - activity" loading="lazy" decoding="async"  />
+            <img src="/assets/activities/hiking2.jpg" alt="Hiking - activity" loading="lazy" decoding="async"  />
+            <img src="/assets/activities/hiking3.jpg" alt="Hiking - activity" loading="lazy" decoding="async"  />
         </section>
         <section className="Stargazing activity-section">
           <h2>Star Gazing</h2><p><i> - Witness a mesmerizing night sky, filled with countless twinkling stars.</i></p>
-            <img src="/assets/activities/stargazing1.jpg" alt="Stargazing - activity" loading="lazy" />
-            <img src="/assets/activities/stargazing2.jpeg" alt="Stargazing - activity" loading="lazy" />
+            <img src="/assets/activities/stargazing1.jpg" alt="Stargazing - activity" loading="lazy" decoding="async"  />
+            <img src="/assets/activities/stargazing2.jpeg" alt="Stargazing - activity" loading="lazy" decoding="async"  />
         </section>
         <section className="Food activity-section">
           <h2>Dining</h2><p><i> -  Savor delicious, freshly prepared meals that fuel your adventure.</i></p>
-            <img src="/assets/activities/food1.jpg" alt="Food - post" loading="lazy" />
-            <img src="/assets/activities/food2.jpg" alt="Food - post" loading="lazy" />
-            <img src="/assets/activities/food3.jpg" alt="Food - post" loading="lazy" />
+            <img src="/assets/activities/food1.jpg" alt="Food - post" loading="lazy" decoding="async"  />
+            <img src="/assets/activities/food2.jpg" alt="Food - post" loading="lazy" decoding="async"  />
+            <img src="/assets/activities/food3.jpg" alt="Food - post" loading="lazy" decoding="async"  />
         </section>
         <section className="Exploringvillages activity-section">
           <h2>Exploring Region</h2><p><i> - Immerse yourself in local culture, history, and traditions as you wander through charming villages.</i></p>
-            <img src="/assets/activities/exploringvillages1.jpg" alt="Exploring Region - activity" loading="lazy" />
-            <img src="/assets/activities/exploringvillages2.jpg" alt="Exploring Region - activity" loading="lazy" />
-            <img src="/assets/activities/exploringvillages3.jpg" alt="Exploring Region - activity" loading="lazy" />
-            <img src="/assets/activities/exploringvillages4.jpg" alt="Exploring Region - activity" loading="lazy" />
-            <img src="/assets/activities/exploringvillages5.jpeg" alt="Exploring Region - activity" loading="lazy" />
+            <img src="/assets/activities/exploringvillages1.jpg" alt="Exploring Region - activity" loading="lazy" decoding="async"  />
+            <img src="/assets/activities/exploringvillages2.jpg" alt="Exploring Region - activity" loading="lazy" decoding="async"  />
+            <img src="/assets/activities/exploringvillages3.jpg" alt="Exploring Region - activity" loading="lazy" decoding="async"  />
+            <img src="/assets/activities/exploringvillages4.jpg" alt="Exploring Region - activity" loading="lazy" decoding="async"  />
+            <img src="/assets/activities/exploringvillages5.jpeg" alt="Exploring Region - activity" loading="lazy" decoding="async"  />
         </section>
         <div></div>
       </div>
